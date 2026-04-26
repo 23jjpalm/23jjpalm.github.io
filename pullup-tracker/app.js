b8bae60fcf4ca280575499826703fcaf26a7933f
23jjpalm <jordanpalmieri03@gmail.com>
2026-04-25 20:22:08 -0400

    Update app.js

--- pullup-tracker/app.js
+++ pullup-tracker/app.js
@@ -62,7 +62,7 @@ async function sendToGoogleSheet(entry) {
       method: "POST",
       mode: "no-cors",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "text/plain;charset=utf-8"
       },
       body: JSON.stringify(entry)
     });
